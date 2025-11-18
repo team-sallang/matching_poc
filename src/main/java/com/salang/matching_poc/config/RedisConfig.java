be56518dd8a6da10a559758397bb57035666e7db
Jungkihong07 <hnn07185@gmail.com>
2025-11-19 07:50:28 +0900

    Redis 설정

---
+++ src/main/java/com/salang/matching_poc/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.salang.matching_poc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public ZSetOperations<String, String> zSetOperations(RedisTemplate<String, String> redisTemplate) {
+        return redisTemplate.opsForZSet();
+    }
+}
+
